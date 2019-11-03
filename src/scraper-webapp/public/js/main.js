1857cae926fa724da32f037549b965709bee92aa
talpenguin <39221045+talpenguin@users.noreply.github.com>
2019-11-03 19:11:42 +0100

    changes to gui, contact and about

--- src/scraper-webapp/public/js/main.js
+++ src/scraper-webapp/public/js/main.js
@@ -1,52 +1,54 @@
 function AutoScrap() {
-    var checkBox = document.getElementById("autoScrap");
-    var text = document.getElementsByClassName("inputfields");
-    var select = document.getElementsByClassName("select") ;
-    var daily = text[0]
-    var weekly = text[1]
-    if (checkBox.checked) {
-        daily.disabled = false;
-        weekly.disabled = false;
-
-    }
-    else {
-        daily.disabled = true;
-        weekly.disabled = true;
-        select[0].disabled = true;
-        select[1].disabled= true;
-    }
+  var checkBox = document.getElementById("autoScrap");
+  var text = document.getElementsByClassName("inputfields");
+  var select = document.getElementsByClassName("select");
+  var daily = text[0];
+  var weekly = text[1];
+  if (checkBox.checked) {
+    daily.disabled = false;
+    weekly.disabled = false;
+  } else {
+    daily.disabled = true;
+    weekly.disabled = true;
+    select[0].disabled = true;
+    select[1].disabled = true;
+  }
 }
 
-
 function dayPick() {
-    var checkBox = document.getElementsByClassName("inputfields")[0];
-    console.log(checkBox)
-    var text = document.getElementsByClassName("select")[0];
-    console.log(text)
+  var checkBox = document.getElementsByClassName("inputfields")[0];
+  console.log(checkBox);
+  var text = document.getElementsByClassName("select")[0];
+  console.log(text);
 
-    if (checkBox.checked) {
-        text.disabled = false;
-        document.getElementsByClassName("select")[1].disabled = true;
-    }
-    else {
-        text.disabled = true;
-    }
+  if (checkBox.checked) {
+    text.disabled = false;
+    document.getElementsByClassName("select")[1].disabled = true;
+  } else {
+    text.disabled = true;
+  }
 }
 
 function weekPick() {
-    var checkBox = document.getElementsByClassName("inputfields")[1];
-    console.log(checkBox)
-    var text = document.getElementsByClassName("select")[1];
-    console.log(text)
+  var checkBox = document.getElementsByClassName("inputfields")[1];
+  console.log(checkBox);
+  var text = document.getElementsByClassName("select")[1];
+  console.log(text);
 
-    if (checkBox.checked) {
-        text.disabled = false;
-        document.getElementsByClassName("select")[0].disabled = true;
+  if (checkBox.checked) {
+    text.disabled = false;
+    document.getElementsByClassName("select")[0].disabled = true;
+  } else {
+    text.disabled = true;
+  }
+}
+function HandleBrowseClick() {
+  var fileinput = document.getElementById("browse");
+  fileinput.click();
+}
 
-
-
-    }
-    else {
-        text.disabled = true;
-    }
+function Handlechange() {
+  var fileinput = document.getElementById("browse");
+  var textinput = document.getElementById("filename");
+  textinput.value = fileinput.value;
 }
