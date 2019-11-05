a835145e1cf5895cb28a692511621d3b54a8eb99
eloraschoerverth <elora666sdg@gmail.com>
2019-11-05 13:56:55 +0100

    cleaned depricated files

--- src/scraper-webapp/scraper.js
+++ src/scraper-webapp/scraper.js
@@ -1,7 +1,6 @@
 const se_scraper = require('se-scraper');
 const dow = require('./download')
 
-<<<<<<< HEAD
 /*This module uses the se-scraper to perform the yandex scraping
 @author Elora
 Params:
@@ -11,10 +10,8 @@ Params:
 */
 exports.yscrape = async function (searchInput, searchDomain, downloadCheck) {
     //Configuration for the scrape_job
-=======
 //This module uses the se-scraper to perform the yandex scraping
 exports.yscrape = async function (searchInput, ru) {
->>>>>>> 03fb627fffc85a3b4b4e7c9d5df0eca0d337e9cf
     let scrape_job = {
         search_engine: 'yandex',
         keywords: [searchInput],
@@ -26,7 +23,6 @@ exports.yscrape = async function (searchInput, ru) {
 
     var results = await se_scraper.scrape({}, scrape_job);
 
-<<<<<<< HEAD
     //checks if the download option has been check off. If so calls the download function.
     if(Boolean(downloadCheck)){
       try{
@@ -39,8 +35,6 @@ exports.yscrape = async function (searchInput, ru) {
     }
 
     console.dir(results, {depth: null, colors: true});
-=======
     console.dir(results, {depth: null, colors: true});
     console.log(ru)
->>>>>>> 03fb627fffc85a3b4b4e7c9d5df0eca0d337e9cf
 }
