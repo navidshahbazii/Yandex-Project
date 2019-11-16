320bbe3c9e5b5307447f1390839c96cf5a103507
talpenguin <39221045+talpenguin@users.noreply.github.com>
2019-11-16 19:57:30 +0100

    new features to mainPage

--- src/scraper-webapp/public/js/main.js
+++ src/scraper-webapp/public/js/main.js
@@ -1,20 +1,26 @@
+//This function disables/enables select inputs according to auto checkbox
 function AutoScrap() {
   var checkBox = document.getElementById("autoScrap");
   var text = document.getElementsByClassName("inputfields");
   var select = document.getElementsByClassName("select");
   var daily = text[0];
   var weekly = text[1];
+  var datePicker = document.getElementById("txtDate");
+
   if (checkBox.checked) {
     daily.disabled = false;
     weekly.disabled = false;
+    datePicker.disabled = false;
   } else {
     daily.disabled = true;
     weekly.disabled = true;
+    datePicker.disabled = true;
     select[0].disabled = true;
     select[1].disabled = true;
   }
 }
 
+//This function disables/enables dropdown of day pick according to select input
 function dayPick() {
   var checkBox = document.getElementsByClassName("inputfields")[0];
   console.log(checkBox);
@@ -29,6 +35,7 @@ function dayPick() {
   }
 }
 
+//This function disables/enables dropdown of week pick according to select input
 function weekPick() {
   var checkBox = document.getElementsByClassName("inputfields")[1];
   console.log(checkBox);
@@ -42,6 +49,8 @@ function weekPick() {
     text.disabled = true;
   }
 }
+
+//functions for input field- add file in addition to normal text input field
 function HandleBrowseClick() {
   var fileinput = document.getElementById("browse");
   fileinput.click();
@@ -52,3 +61,21 @@ function Handlechange() {
   var textinput = document.getElementById("filename");
   textinput.value = fileinput.value;
 }
+
+// Function to restrict calender(for future dates only and one year limit!)
+$(function() {
+  var dtToday = new Date();
+
+  var month = dtToday.getMonth() + 1;
+  var day = dtToday.getDate();
+  var year = dtToday.getFullYear();
+  var nextYear = dtToday.getFullYear() + 1;
+  if (month < 10) month = "0" + month.toString();
+  if (day < 10) day = "0" + day.toString();
+
+  var minDate = year + "-" + month + "-" + day;
+  var maxDate = nextYear + "-" + month + "-" + day;
+
+  $("#txtDate").attr("min", minDate);
+  $("#txtDate").attr("max", maxDate);
+});
